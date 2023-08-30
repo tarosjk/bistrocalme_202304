9b2adb73709e8238547f6e85b10e23d9044474e8
taro sjk <tarosjk@gmail.com>
2023-08-30 11:58:17 +0900

    single.php 本文、前後の記事リンクの設定

--- wp-content/themes/bistro-calme/single.php
+++ wp-content/themes/bistro-calme/single.php
@@ -7,32 +7,34 @@
 		<div class="row">
 			<div class="col-12 col-md-9">
 
-				<article class="article">
-					<header class="article_header">
-						<h2 class="article_title">タイトルタイトルタイトル</h2>
-						<div class="article_meta">
-							<ul class="post-categories">
-								<li><a href="#">お知らせ</a></li>
-							</ul>
-							<time datetime="2019-1-1">2019年1月1日</time>
-						</div>
-					</header>
+				<?php if (have_posts()) : ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<article id="post-<?php the_ID() ?>" <?php post_class('article') ?>>
+							<header class="article_header">
+								<h2 class="article_title"><?php the_title() ?></h2>
+								<div class="article_meta">
+									<?php the_category() ?>
+									<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('Y年m月d日') ?></time>
+								</div>
+							</header>
 
-					<div class="article_body">
-						<div class="content">
-							<p>BISTRO CALME では、新しいメニューを開発中です。</p>
-							<p>
-								新しいメニューを提供するために、開店後に練習中です。スタッフから大好評のメニューもできてきました。<br>
-								来月にはご提供できると思います。お楽しみに。
-							</p>
-						</div>
-					</div>
+							<div class="article_body">
+								<div class="content">
+									<?php the_content() ?>
+								</div>
+							</div>
 
-					<div class="postLinks">
-						<div class="postLink postLink-prev"><a href="#"><i class="fas fa-chevron-left"></i>前の記事のタイトル</a></div>
-						<div class="postLink postLink-next"><a href="#">次の記事のタイトル<i class="fas fa-chevron-right"></i></a></div>
-					</div>
-				</article>
+							<div class="postLinks">
+								<div class="postLink postLink-prev">
+									<?php previous_post_link('%link', '<i class="fas fa-chevron-left"></i>%title') ?>
+								</div>
+								<div class="postLink postLink-next">
+									<?php next_post_link('%link', '%title<i class="fas fa-chevron-right"></i>') ?>
+								</div>
+							</div>
+						</article>
+					<?php endwhile; ?>
+				<?php endif; ?>
 
 			</div>
 
