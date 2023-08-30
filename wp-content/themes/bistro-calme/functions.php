f551090eadc11f2e760502dbcd79b43b5f561981
taro sjk <tarosjk@gmail.com>
2023-08-30 11:01:15 +0900

    アイキャッチ画像の設定 WordPressループの完成

--- wp-content/themes/bistro-calme/functions.php
+++ wp-content/themes/bistro-calme/functions.php
@@ -2,6 +2,8 @@
 
 // titleタグを出力する
 add_theme_support('title-tag');
+// アイキャッチ画像を有効化する
+add_theme_support('post-thumbnails');
 
 add_filter('document_title_separator', 'my_document_title_separator');
 function my_document_title_separator()
--- wp-content/themes/bistro-calme/index.php
+++ wp-content/themes/bistro-calme/index.php
@@ -25,20 +25,22 @@
 					<div class="col-md-4">
 						<article id="post-<?php the_ID() ?>" <?php post_class('news') ?>>
 							<div class="news_pic">
-								<a href="#">
-									<img src="<?= get_template_directory_uri() ?>/assets/img/home/news_img-1.jpg" alt="">
+								<a href="<?php the_permalink() ?>">
+								<?php if(has_post_thumbnail()): ?>
+									<?php the_post_thumbnail() ?>
+								<?php else: ?>
+									<img src="<?= get_template_directory_uri() ?>/assets/img/common/noimage_600x400.png" alt="">
+								<?php endif; ?>
 								</a>
 							</div>
 							<div class="news_meta">
-								<ul class="post-categories">
-									<li><a href="#">お知らせ</a></li>
-								</ul>
-								<time class="news_time" datetime="2019-00-00">2019年00月00日</time>
+								<?php the_category() ?>
+								<time class="news_time" datetime="<?php the_time('Y-m-d') ?>"><?php the_time('Y年m月d日') ?></time>
 							</div>
-							<h2 class="news_title"><a href="#">タイトルタイトルタイトルタイトル</a></h2>
+							<h2 class="news_title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 							<div class="news_desc">
-								<p>概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。</p>
-								<p><a href="#">[続きを読む]</a></p>
+								<?php the_excerpt() ?>
+								<p><a href="<?php the_permalink() ?>">[続きを読む]</a></p>
 							</div>
 						</article>
 					</div>
