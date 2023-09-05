2de488ce5e709467630c65bfb43f2083cc9daa3d
taro sjk <tarosjk@gmail.com>
2023-09-05 14:42:46 +0900

    メニューページ作成完了

--- wp-content/themes/bistro-calme/archive-menu.php
+++ wp-content/themes/bistro-calme/archive-menu.php
@@ -1,75 +1,74 @@
 <?php get_header() ?>
 
 <h2 class="pageTitle">メニュー<span>MENU</span></h2>
+
 <?php get_template_part('template-parts/breadcrumb') ?>
 
+<?php
+$kinds = get_terms([
+  'taxonomy' => 'kind',
+  'hide_empty' => false,
+  'order' => 'DESC',
+]);
+
+?>
+
+<?php if (!empty($kinds)) : ?>
+  <div class="pageNav">
+    <ul>
+      <?php foreach ($kinds as $kind) : ?>
+        <li>
+          <a href="<?= get_term_link($kind) ?>">
+            <?= $kind->name ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+<?php endif; ?>
+
 <main class="main">
-  <section class="sec">
-    <div class="container">
-      <div class="sec_header">
-        <h2 class="title title-jp">フード</h2>
-        <span class="title title-en">FOOD</span>
-      </div>
-      <div class="row justify-content-center">
 
-        <?php if (have_posts()) : ?>
-          <?php while (have_posts()) : the_post(); ?>
-            <div class="col-md-3">
-              <?php get_template_part('template-parts/loop', 'menu') ?>
-            </div>
-          <?php endwhile ?>
-        <?php endif ?>
-
-      </div>
-    </div>
-  </section>
-
-
-  <section class="sec">
-    <div class="container">
-      <div class="sec_header">
-        <h2 class="title title-jp">ドリンク・デザート</h2>
-        <span class="title title-en">DRINK-DESSERT</span>
-      </div>
-      <div class="row justify-content-center">
-
-        <div class="col-md-3">
-          <section class="menu">
-            <a href="#">
-              <figure class="menu_pic">
-                <img src="./assets/img/menu/menu-9@2x.jpg" alt="">
-              </figure>
-              <h3 class="menu_title">メニュー名が入ります</h3>
-              <p class="menu_price">800円</p>
-              <div class="menu_desc">
-                <p>
-                  概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                </p>
-              </div>
-            </a>
-          </section>
+  <?php foreach ($kinds as $kind) : ?>
+    <section class="sec">
+      <div class="container">
+        <div class="sec_header">
+          <h2 class="title title-jp"><?= $kind->name ?></h2>
+          <span class="title title-en">
+            <?= strtoupper($kind->slug) ?>
+          </span>
         </div>
+        <div class="row justify-content-center">
+          <?php
+          $args = [
+            'post_type' => 'menu',
+            'posts_per_page' => -1, //-1は全件
+            'tax_query' => [
+              'relation' => 'AND',
+              [
+                'taxonomy' => $kind->taxonomy,
+                'field' => 'slug',
+                'terms' => $kind->slug,
+              ]
+            ]
+          ];
 
-        <div class="col-md-3">
-          <section class="menu">
-            <a href="#">
-              <figure class="menu_pic">
-                <img src="./assets/img/menu/menu-10@2x.jpg" alt="">
-              </figure>
-              <h3 class="menu_title">メニュー名が入ります</h3>
-              <p class="menu_price">800円</p>
-              <div class="menu_desc">
-                <p>
-                  概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                </p>
+          $the_query = new WP_Query($args);
+          ?>
+          <?php if ($the_query->have_posts()) : ?>
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+              <div class="col-md-3">
+                <?php get_template_part('template-parts/loop', 'menu') ?>
               </div>
-            </a>
-          </section>
-        </div>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+          <?php endif ?>
 
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  <?php endforeach; ?>
+
 </main>
 
 <?php get_footer() ?>
